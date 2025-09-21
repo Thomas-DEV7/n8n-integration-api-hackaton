f0e83f8b606f331c6cb62959074352b5082e7a40
Thomas Bastos <thomas.felip16@gmail.com>
2025-09-21 03:16:12 -0300

    chore: mount save routes into main router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,9 @@
-
 import { Router } from "express";
 import n8n from "./n8n.js";
+import save from "./save.js";
 
 const router = Router();
 router.use(n8n);
+router.use(save);
 
 export default router;
