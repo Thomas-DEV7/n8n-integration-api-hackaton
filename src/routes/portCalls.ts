b9d48b98b14436d077389e86f71edeac22788551
Thomas Bastos <thomas.felip16@gmail.com>
2025-09-21 06:06:08 -0300

    feat: add log

--- src/routes/portCalls.ts
+++ src/routes/portCalls.ts
@@ -2,10 +2,11 @@
 import { Router } from "express";
 import { requireApiKey } from "../middlewares/apiKey.js";
 import { listPortCalls } from "../controllers/list.controller.js";
+import { reqlog } from "../middlewares/reqlog.js";
 
 const router = Router();
 
 // GET /api/v1/n8n/port-calls
-router.get("/n8n/port-calls", requireApiKey, listPortCalls);
+router.get("/n8n/port-calls", requireApiKey,reqlog, listPortCalls);
 
 export default router;
--- src/routes/save.ts
+++ src/routes/save.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { requireApiKey } from "../middlewares/apiKey.js";
 import { savePortCalls } from "../controllers/save.controller.js";
+import { reqlog } from "../middlewares/reqlog.js";
 
 const router = Router();
-router.post("/n8n/save", requireApiKey, savePortCalls);
+router.post("/n8n/save", requireApiKey,reqlog, savePortCalls);
 export default router;
